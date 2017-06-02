b65697c020700fc93ae3188df0ab61d90b8d70af
Kelvin Quinonez <kesqui7@gmail.com>
2017-06-02 08:38:25 -0500

    A new controller was added for the types of mushrooms. The views were also added for that controller, there is an index view and add view. I will tie these into the mushroom activity controller so that we can add different types of mushrooms to the database if that type is not existent yet. Or the user will also be able to select a type of mushroom from a drop down that has existing mushroom types.

---
+++ src/BetaMushroom/Controllers/TypeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BetaMushroom.Models;
+using BetaMushroom.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using BetaMushroom.Data;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BetaMushroom.Controllers
+{
+    public class TypeController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public TypeController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+         
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<MushroomType> types = context.Types.ToList();
+            return View(types);
+        }
+
+        public IActionResult Add()
+        {
+            AddTypeViewModel addTypeViewModel = new AddTypeViewModel();
+            return View(addTypeViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTypeViewModel addTypeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var theType = addTypeViewModel.Name;
+                MushroomType newType = new MushroomType
+                {
+                    Name = theType
+                };
+
+                context.Types.Add(newType);
+                context.SaveChanges();
+                return Redirect("/Types");
+            }
+
+            return View(addTypeViewModel);
+        }
+    }
+}
